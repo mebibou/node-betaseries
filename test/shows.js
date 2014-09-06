b0d0b95de7c8b4e59131f06ed9f7e44ee1681ab7
groyer <guillaumeroyer.mail@gmail.com>
2014-09-06 18:53:08 +0200

    feat(shows): create all methods for /shows endpoints + create tests

---
+++ test/shows.js
@@ -0,0 +1,25 @@
+var assert = require('chai').assert,
+    BetaSeries = require('../index');
+
+describe('#valid', function() {
+  var betaSeries = new BetaSeries('e426aad8d05f');
+  var shows = betaSeries.Shows;
+
+  it('get info about a show', function(done) {
+    var showId = 10;
+
+    shows.get(showId, null, function(response) {
+      assert(response.id == showId, 'show exists');
+      assert(response.title == 'Prison Break', 'show is Prison Break');
+      done();
+    });
+  });
+
+  it('search shows', function(done) {
+    shows.search('breaking', null, null, null, null, function(response) {
+      assert(response.length > 1, 'found shows');
+      done();
+    });
+  });
+
+});
